4bc820399eddb2a5c51bec6f1a5b75f837d7b0d3
FredyyLima <fredyy_lima@live.com>
2022-05-21 00:03:03 -0300

    .

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__, template_folder='templates')
 mail_settings = {
     "MAIL_SERVER": 'mail.eutatilima.com.br',
     "MAIL_PORT": 587,
-    "MAIL_USE_TLS": False,
+    "MAIL_USE_TLS": True,
     "MAIL_USE_SSL": False,
     "MAIL_USERNAME": username,
     "MAIL_PASSWORD": senha
